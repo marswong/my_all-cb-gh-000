e1de1933b89ccbd0e1aa035bf14455f15f185257
Mars Wong <marswong618@gmail.com>
2018-01-26 02:52:54 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,19 @@
 require 'pry'
 
 def my_all?(collection)
-
+  if block_given?
+    i = 0
+    result = true
+    while i < collection.length
+      if yield(collection[i])
+        i += 1
+      else
+        result = false
+        break
+      end
+    end
+    result
+  else
+    throw SyntaxError
+  end
 end
