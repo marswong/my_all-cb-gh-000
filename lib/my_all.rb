910cb8790fec1957d2fec5192ef50b841a1d13bd
Mars Wong <marswong618@gmail.com>
2018-01-26 02:48:28 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,6 +2,12 @@ require 'pry'
 
 def my_all?(collection)
   if block_given?
+    i = 0
+    while i < collection.length
+      if yield(collection[i])
+        i += 1
+      end
+    end
   else
     throw SyntaxError
   end
